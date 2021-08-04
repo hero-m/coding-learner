ea9ba1927946fc4506e0fb4a8e92da9f5c36d529
herom <milad.b.patterson@gmail.com>
2021-08-05 01:15:19 +0430

    use lists

--- convert-digit/main.py
+++ convert-digit/main.py
@@ -1,49 +1,13 @@
+ones_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+eleventy_list = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+tens_list = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
 def ones_str(x):
-    if x == 0:
-        digit_str = "zero"
-    if x == 1:
-        digit_str = "one"
-    if x == 2:
-        digit_str = "two"
-    if x == 3:
-        digit_str = "three"
-    if x == 4:
-        digit_str = "four"
-    if x == 5:
-        digit_str = "five"
-    if x == 6:
-        digit_str = "six"
-    if x == 7:
-        digit_str = "seven"
-    if x == 8:
-        digit_str = "eight"
-    if x == 9:
-        digit_str = "nine"
-    return digit_str
+    return ones_list[x]
 
 
 def eleventy_str(x):
-    if x == 0:
-        digit_str = "ten"
-    if x == 1:
-        digit_str = "eleven"
-    if x == 2:
-        digit_str = "twelve"
-    if x == 3:
-        digit_str = "thirteen"
-    if x == 4:
-        digit_str = "fourteen"
-    if x == 5:
-        digit_str = "fifteen"
-    if x == 6:
-        digit_str = "sixteen"
-    if x == 7:
-        digit_str = "seventeen"
-    if x == 8:
-        digit_str = "eighteen"
-    if x == 9:
-        digit_str = "nineteen"
-    return digit_str
+    return eleventy_list[x]
 
 
 def tens_str(x):
@@ -55,22 +19,7 @@ def tens_str(x):
     elif digit2 == 1:
         digit_str = eleventy_str(digit1)
     else:
-        if digit2 == 2:
-            digit_str = "twenty"
-        if digit2 == 3:
-            digit_str = "thity"
-        if digit2 == 4:
-            digit_str = "forty"
-        if digit2 == 5:
-            digit_str = "fifty"
-        if digit2 == 6:
-            digit_str = "sixty"
-        if digit2 == 7:
-            digit_str = "seventy"
-        if digit2 == 8:
-            digit_str = "eighty"
-        if digit2 == 9:
-            digit_str = "ninety"
+        digit_str = tens_list[digit2]
 
         if digit1 != 0:
             digit_str = digit_str + " " + ones_str(digit1)
@@ -85,24 +34,7 @@ def hundreds_str(x):
     if digit3 == 0:
         digit_str = tens_str(rest)
     else:
-        if digit3 == 1:
-            digit_str = "one hundred"
-        if digit3 == 2:
-            digit_str = "two hundred"
-        if digit3 == 3:
-            digit_str = "three hundred"
-        if digit3 == 4:
-            digit_str = "four hundred"
-        if digit3 == 5:
-            digit_str = "five hundred"
-        if digit3 == 6:
-            digit_str = "six hundred"
-        if digit3 == 7:
-            digit_str = "seven hundred"
-        if digit3 == 8:
-            digit_str = "eight hundred"
-        if digit3 == 9:
-            digit_str = "nine hundred"
+        digit_str = ones_list[digit3] + " hundred"
 
         if rest != 0:
             digit_str = digit_str + " and " + tens_str(rest)
@@ -117,24 +49,7 @@ def thousands_str(x):
     if digit4 == 0:
         digit_str = hundreds_str(rest)
     else:
-        if digit4 == 1:
-            digit_str = "one thousand"
-        if digit4 == 2:
-            digit_str = "two thousand"
-        if digit4 == 3:
-            digit_str = "three thousand"
-        if digit4 == 4:
-            digit_str = "four thousand"
-        if digit4 == 5:
-            digit_str = "five thousand"
-        if digit4 == 6:
-            digit_str = "six thousand"
-        if digit4 == 7:
-            digit_str = "seven thousand"
-        if digit4 == 8:
-            digit_str = "eight thousand"
-        if digit4 == 9:
-            digit_str = "nine thousand"
+        digit_str = ones_list[digit4] + " thousand"
 
         if rest != 0:
             digit_str = digit_str + " and " + hundreds_str(rest)
